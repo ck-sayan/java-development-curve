4ae146916ad5eb2afd2b9ed6c2a761439d09b352
ck-sayan <ck.sayan2002@gmail.com>
2024-07-31 10:39:19 +0530

    Added the concepts of while and do-while loops.

---
+++ code-track/level-basics/do_while_loop.java
@@ -0,0 +1,16 @@
+public class do_while_loop {
+    public static void main(String args[]){
+
+        /* do{
+         //do something
+        } while(condition); */
+
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<101);
+    }
+    
+}
